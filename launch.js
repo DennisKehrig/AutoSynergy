22324a2349f5d3d051dff118eb2a415f5651dc2a
Dennis Kehrig <mailgithub@denniskehrig.de>
2012-08-09 13:50:27 +0200

    Allow a delayed start. Somehow fixes problems when AutoSynergy is in the Autorun folder

--- launch.js
+++ launch.js
@@ -1,2 +1,14 @@
-require('coffee-script');
-require('./index');
+var delay = 0;
+
+if (process.argv.length > 2) {
+	var delay = parseInt(process.argv[2], 10);
+	if (isNaN(delay)) {
+		console.log(process.argv[2] + " is not a proper number");
+		process.exit();
+	}
+}
+
+setTimeout(function() {
+	require('coffee-script');
+	require('./index');
+}, delay * 1000);
